a6251cd43ba487a79e9d15559b71a35decbd4bb6
Floki <alexdotnet@163.com>
2022-05-26 22:55:43 +0800

    add core&framework

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# ratel-go
-this is a web framework of golang
+# <img src="http://rchvpjxjd.hn-bkt.clouddn.com/logo.png" style="width:100px;">
+# Ratel-Go
+It is a web framework of golang
---
+++ framework/context.go
@@ -0,0 +1,53 @@
+package framework
+
+import (
+	"context"
+	"net/http"
+	"sync"
+	"time"
+)
+
+type Context struct {
+	request        *http.Request
+	responseWriter http.ResponseWriter
+	ctx            context.Context
+	handler        ControllerHandler
+	hasTimeOut     bool
+	writeMux       *sync.Mutex
+}
+
+func NewContext(r *http.Request, w http.ResponseWriter) *Context {
+	return &Context{request: r, responseWriter: w, ctx: r.Context(), writeMux: &sync.Mutex{}}
+}
+
+func (c *Context) WriteMux() *sync.Mutex {
+	return c.writeMux
+}
+
+func (c *Context) GetRequest() *http.Request {
+	return c.request
+}
+
+func (c *Context) GetResponse() http.ResponseWriter {
+	return c.responseWriter
+}
+
+func (c *Context) SetTimeOut() {
+	c.hasTimeOut = true
+}
+
+func (c *Context) HasTimeOut() bool {
+	return c.hasTimeOut
+}
+
+func (c *Context) BaseContext() context.Context {
+	return c.request.Context()
+}
+
+func (c *Context) Deadline() (deadline time.Time, ok bool) {
+	return c.BaseContext().Deadline()
+}
+
+func (c *Context) Done() <-chan struct{} {
+	return c.BaseContext().Done()
+}
